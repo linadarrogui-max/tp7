e38ffb59245b9a61126b67f3273c35702dd06aae
linadarrogui-max <l.darrogui@esisa.ac.ma>
2026-05-18 15:16:51 +0100

    initialisation projet

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const signup = document.getElementById('signup');
 
-alert('welcome to the signup page!');
 console.log(name);
 console.log(email);
 console.log(password);
-console.log(button);
-button.addEventListener('click', () => {
+console.log(signup);
+
+signup.addEventListener('click', () => {
     console.log('button clicked!');
     console.log(name.value);
     console.log(email.value);
     console.log(password.value);
-    alert('name' + name.value + "\nemail" + email.value + "\npassword" + password.value);
+
+    alert(
+        'name: ' + name.value +
+        "\nemail: " + email.value +
+        "\npassword: " + password.value
+    );
 });
